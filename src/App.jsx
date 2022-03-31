094918fc7f8950a28bfe0046af808e2a31f07512
normuradoff <normuradoff1@gmail.com>
2022-03-31 12:50:16 +0500

    remove typescript

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import { Layout } from "./layout/Layout";
+
+const App = () => {
+  return <Layout>hello</Layout>;
+};
+
+export default App;
--- src/App.tsx
+++
@@ -1,7 +0,0 @@
-import type { Component } from "solid-js";
-
-const App: Component = () => {
-  return null;
-};
-
-export default App;
