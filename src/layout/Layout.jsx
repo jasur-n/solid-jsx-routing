26553ab6a4a2236641496f9548d440925e2dea56
normuradoff <normuradoff1@gmail.com>
2022-03-31 12:50:06 +0500

    add layout component

---
+++ src/layout/Layout.jsx
@@ -0,0 +1,14 @@
+import { NavLink } from "solid-app-router";
+
+export const Layout = ({ children }) => {
+  return (
+    <div>
+      <nav class="flex gap-2">
+        <NavLink href="/">Home</NavLink>
+        <NavLink href="/movies">Movies</NavLink>
+        <NavLink href="/tv">TV</NavLink>
+      </nav>
+      <div>{children}</div>
+    </div>
+  );
+};
